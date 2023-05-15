633ac63b4bb369631cc9a28fe334197185bef1f1
Jairo Alonso Abreu <alu0101330778@ull.edu.es>
2023-05-15 11:43:56 +0000

    Terminado

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 
-const MONGO_URI = 'mongodb://127.0.0.1:27017/recuperacion';
-const MongoURL = 'mongodb+srv://notes-rest-api:notesDSI@cluster0.rypap0v.mongodb.net/recuperacion'
+//const MONGO_URI = 'mongodb://127.0.0.1:27017/recuperacion';
+const MongoURL = process.env.MongoURL ||'mongodb+srv://notes-rest-api:notesDSI@cluster0.rypap0v.mongodb.net/recuperacion'
 
 connect(MongoURL).then(() => {
   console.log('Connection to MongoDB server established');
